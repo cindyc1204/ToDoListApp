8744c16d4a71edc2f622155d5560f55b33c55d8f
Cindy <cindyczy061204@gmail.com>
2022-08-15 18:18:34 -0400

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Zhiyi Chen on 8/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
